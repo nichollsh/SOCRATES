Unit conversion fixes. Now reads NetCDF directly. Now reads ExoMol files

spectraltools/common/cross.py
@@ -130,7 +130,7 @@ class xsec():
         raw_data = np.array([],dtype=float)
         for b in body:
             raw_data = np.append(raw_data, [float(v) for v in b.split()])
-        self.arr_k = raw_data * phys.N_av / (self.mmw * 1000.0)
+        self.arr_k = raw_data * phys.N_av / (self.mmw * 1000.0)  # cm2/molec -> cm2/gram
 
         # Generate wavenumber grid
         self.nbins = len(self.arr_k)
@@ -139,9 +139,26 @@ class xsec():
         # Flag as loaded 
         self.loaded = True 
 
+    # Parse ExoMol sigma file name
+    def parse_sigmaname(self):
+        if not (self.source == "exomol"):
+            print("WARNING: Cannot execute parse_sigmaname because source (%s) is not ExoMol" % self.source)
+        
+        # Exomol values are always at zero pressure
+        self.p = 0.0
+
+        # Process filename
+        splt = self.fname[:-6].split("_")
+        splt_nu = splt[1].split("-")
+
+        self.numin = float(splt_nu[0]) 
+        self.numax = float(splt_nu[1]) 
+        self.t     = float(splt[2][:-1])
+
+
     # Read ExoMol sigma file
     def readsigma(self):
-        if not (self.source == "hitran"):
+        if not (self.source == "exomol"):
             print("WARNING: Cannot execute readsigma because source (%s) is not ExoMol" % self.source)
 
         # check conflicts
@@ -149,16 +166,44 @@ class xsec():
             raise Exception("This xsec object already contains data")
         if not os.access(self.fname, os.R_OK):
             raise Exception("Cannot read file '%s'" % self.fname)
+    
+        self.parse_sigmaname()
 
-        raise Exception("Function readsigma() is not yet implemented!")
+        data = np.loadtxt(self.fname).T 
+        self.arr_nu = data[0]
+        self.arr_k  = data[1] * phys.N_av / (self.mmw * 1000.0) 
+        self.nbins  = len(data[0])
+
+        # Flag as loaded
+        self.loaded = True
+
+
+    # Read input variables instead of from a file (bar, K, cm-1, cm2/g)
+    def readdirect(self, p, t, nu_arr, k_arr):
+        # check conflicts
+        if self.loaded:
+            raise Exception("This xsec object already contains data")
+
+        if len(nu_arr) != len(k_arr):
+            raise Exception("nu and k arrays have different lengths")
+
+        self.p = p 
+        self.t = t
+        self.arr_nu = nu_arr 
+        self.numin = nu_arr[0]
+        self.numax = nu_arr[1]
+        self.nbins = len(nu_arr)
+        self.arr_k = k_arr
+        self.loaded = True
+
         
     # Read source file 
-    def read(self):
+    def read(self, p=None, t=None, nu_arr=None, k_arr=None):
         match self.source:
             case "dace":   self.readbin()
             case "hitran": self.readxsc()
             case "exomol": self.readsigma()
-
+            case "direct": self.readdirect(p,t,nu_arr,k_arr)
 
     # Return cross-section in units of cm2.molecule-1
     def cross_cm2_per_molec(self):

spectraltools/common/dace.py
@@ -46,20 +46,16 @@ def find_bin_close(directory:str, p_aim:float, t_aim:float) -> str:
     
     p_arr = []  # pressure
     t_arr = []  # temperature
-    d_arr = []  # distance from target
     for f in files:
         temp = cross.xsec("", "dace", f)
         temp.parse_binname()
         p_arr.append(temp.p)
         t_arr.append(temp.t)
 
-        dist = 100.0 * ( ( (p_aim-temp.p)/temp.p )**2.0 + ( (t_aim-temp.t)/temp.t )**2.0 ) ** 0.5
-        d_arr.append(dist)
+    i,d,p,t = utils.find_pt_close(p_arr, t_arr, p_aim, t_aim)
+    print("Found BIN file with distance = %.3f%%" % d)
 
-    i_close = np.argmin(d_arr)
-    print("Found BIN file with distance = %.3f%%" % d_arr[i_close])
-
-    return files[i_close]
+    return files[i]
 
 def find_grid(directory:str, p_list:list, t_list:list):
     """Get DACE files that are close to the given p,t values.

spectraltools/common/netcdf.py
@@ -91,7 +91,7 @@ def write_ncdf(formula:str, source:str, p_points:np.ndarray, t_points:np.ndarray
         # Read file at this p,t
         this_xsec = cross.xsec(formula, source, f_points[i])
         this_xsec.read()
-        var_xc[i,:] = this_xsec.arr_k * 10.0  # convert cm2/g to m2/kg
+        var_xc[i,:] = this_xsec.arr_k / 10.0  # convert cm2/g to m2/kg
         del this_xsec
 
     print("    done writing to '%s'" % ds_path)
@@ -99,5 +99,55 @@ def write_ncdf(formula:str, source:str, p_points:np.ndarray, t_points:np.ndarray
     ds.close()
     return ds_path
 
+def read_netcdf_pt(fpath:str):
+    """Read p,t values from netCDF file
 
-    
+    Parameters
+    ----------
+    fpath : str
+        Path to netCDF file
+
+    Returns
+    -------
+    np.ndarray
+        Sorted pressure values [bar]
+    np.ndarray 
+        Sorted temperature values [K]
+    """
+
+    ds = Dataset(fpath, "r", format="NETCDF4")
+
+    arr_p  = np.array(ds.variables["p_calc"][:] ) * 1.0e-5  # pa to bar
+    arr_t  = np.array(ds.variables["t_calc"][:] )
+
+    ds.close()
+
+    return arr_p, arr_t
+
+def read_netcdf_point(fpath:str, idx:int):
+    """Read netCDF cross-section data at a specific index
+
+    Parameters
+    ----------
+    fpath : str
+        Path to netCDF file
+    idx : int
+        Index of point
+
+    Returns
+    -------
+    np.ndarray
+        Wavenumber array [cm-1]
+    np.ndarray
+        Cross-section array [cm2/g]
+    """
+
+    ds = Dataset(fpath, "r", format="NETCDF4")
+
+    arr_nu = np.array(ds.variables["nu"][:]) / 100.0   # m-1 to cm-1
+    arr_k  = np.array(ds.variables["kabs"][idx][:]) * 10.0  # m2/kg to cm2/g
+
+    ds.close()
+
+    return arr_nu, arr_k
+

spectraltools/common/utils.py
@@ -1,6 +1,7 @@
 # General utilities 
 
 import os
+import numpy as np
 
 # Convert wavenumber [cm-1] to wavelength [nm]
 def wn2wl(wn:float) -> float:
@@ -15,6 +16,16 @@ def wl2wn(wl:float) -> float:
         return float("inf")
     else:
         return 10000000.0 / wl
+    
+# Find the closest point in a p,t grid, returning its index, distance, p, t.
+def find_pt_close(arr_p, arr_t, target_p, target_t):
+    nvals = len(arr_p)
+    dists = []
+    for i in range(nvals):
+        dists.append(100.0 * ( ( (arr_p[i]-target_p)/target_p)**2.0 + (  (arr_t[i]-target_t)/target_t  )**2.0   )**0.5)
+    iclose = np.argmin(dists)
+    dclose = dists[iclose]
+    return iclose, dclose, arr_p[iclose], arr_t[iclose]
     
 # Named directories
 dirs = {"tools":os.path.join(os.path.abspath(os.environ["RAD_DIR"]), "spectraltools/")}
@@ -31,7 +42,7 @@ def check_output_exists():
 # Sanitise source string
 def sourcesafe(source:str):
     safe = source.strip().lower()
-    if safe not in ["dace", "hitran", "exomol"]:
+    if safe not in ["dace", "hitran", "exomol", "direct"]:
         raise Exception("Invalid source '%s'"% source)
     return safe
 